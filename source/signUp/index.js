e7bdd50b055f8be0eafedf3de69eaca1b7bc3f0c
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-07-14 11:57:08 -0700

    Minor changes

--- source/signUp/index.js
+++ source/signUp/index.js
@@ -1,21 +1,35 @@
 import nightmare from "nightmare"
 import {test} from "tap"
+import {pipe} from "ramda"
 
 const WWW_LOCATION = "http://www.lacqueristas.local"
-const configuration = {
-  show: true,
-  alwaysOnTop: false,
-  dock: true,
-  width: 1280,
-  height: 1024,
+const travel = (actions) => pipe(actions)()
+const getReady = () => {
+  return nightmare({
+    show: true,
+    alwaysOnTop: false,
+    dock: true,
+    width: 1280,
+    height: 1024,
+  })
 }
-const open = (path) => {
-  return nightmare(configuration)
-    .cookies.clearAll()
-    .goto(`${WWW_LOCATION}${path}`)
-}
+const clearAllCookies = (client) => client.cookies.clearAll()
+const goTo = (location) => (client) => client.goto(`${WWW_LOCATION}${location}`)
+const goToTheLandingPage = goTo("/")
+const joinUs = (client) => client
+  .then()
+const giveAEmail = (client) => client
+  .then()
+const submitSignUp = (client) => client
+  .then()
 
 test(({equal}) => {
-  return open("/")
-    .then()
+  return travel(
+    getReady,
+    clearAllCookies,
+    goToTheLandingPage,
+    joinUs,
+    giveAEmail,
+    submitSignUp,
+  )
 })
